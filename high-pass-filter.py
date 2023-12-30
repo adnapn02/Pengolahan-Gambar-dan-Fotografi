f7dd8292832132bec36a5aab1f040bab62edb3b7
adnapn02 <adnapandunaesa@gmail.com>
2023-12-30 14:38:44 +0700

    menambah file baru

---
+++ high-pass-filter.py
@@ -0,0 +1,10 @@
+# high pass filter ->
+import cv2
+import numpy as np
+from matplotlib import pyploy as plt
+
+img = cv2.imread("sudoku.jpg", 0)
+laplacian = cv2.Laplacian(img, cv2.CV_64F)
+plt.imshow(laplacian, cmap="gray")
+plt.title("Laplacian"), plt.xticks([]), plt.yticks([])
+plt.show()
